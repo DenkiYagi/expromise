0ea354b9b70ee895d8f7032c05bee6e989b95112
terurou <terurou@denkiyagi.jp>
2020-09-24 18:09:12 +0900

    Fix for completion issue of CancelablePromise

--- src/exasync/PromiseHandler.hx
+++ src/exasync/PromiseHandler.hx
@@ -1,11 +1,13 @@
 package exasync;
 
+import exasync.CancelablePromise;
 #if js
 import js.lib.Promise.PromiseHandler in JsPromiseHandler;
 
 abstract PromiseHandler<T, U>(T->Dynamic)
     from T->U
     from T->Promise<U>
+    from T->CancelablePromise<U>
     to JsPromiseHandler<T, U>
 {
     public inline extern function call(x:T):Dynamic {
@@ -15,6 +17,7 @@ abstract PromiseHandler<T, U>(T->Dynamic)
 abstract PromiseHandler0<T, U>(Void->Dynamic)
     from ()->U
     from ()->Promise<U>
+    from ()->CancelablePromise<U>
     to Void->Dynamic
 {
     @:to function toJsPromiseHandler():JsPromiseHandler<T, U> {
@@ -25,6 +28,7 @@ abstract PromiseHandler0<T, U>(Void->Dynamic)
 abstract PromiseHandler<T, U>(T->Dynamic)
     from T->U
     from T->Promise<U>
+    from T->CancelablePromise<U>
 {
     public inline extern function call(x:T):Dynamic {
         return this(x);
@@ -34,6 +38,7 @@ abstract PromiseHandler<T, U>(T->Dynamic)
 abstract PromiseHandler0<T, U>(Void->Dynamic)
     from ()->U
     from ()->Promise<U>
+    from ()->CancelablePromise<T>
     to Void->Dynamic
 {
 }
