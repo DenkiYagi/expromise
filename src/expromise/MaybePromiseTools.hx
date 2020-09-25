544812d783718f28f6e1df01641334edda704e16
terurou <terurou@denkiyagi.jp>
2020-09-25 17:19:52 +0900

    Add some functions to MaybePromiseTools

--- src/expromise/MaybePromiseTools.hx
+++ src/expromise/MaybePromiseTools.hx
@@ -3,8 +3,40 @@ package expromise;
 import expromise.PromiseHandler;
 import extools.EqualsTools;
 import extype.Maybe;
+import haxe.ds.Option;
 
 class MaybePromiseTools {
+    public static inline function thenToOption<T>(promise:Promise<Maybe<T>>):Promise<Option<T>> {
+        return promise.then(x -> x.toOption());
+    }
+    public static inline function thenGet<T>(promise:Promise<Maybe<T>>):Promise<Null<T>> {
+        return promise.then(x -> x.get());
+    }
+
+    public static inline function thenGetUnsafe<T>(promise:Promise<Maybe<T>>):Promise<T> {
+        return promise.then(x -> x.getUnsafe());
+    }
+
+    public static inline function thenGetOrThrow<T>(promise:Promise<Maybe<T>>, ?errorFn:() -> Dynamic):Promise<T> {
+        return promise.then(x -> x.getOrThrow(errorFn));
+    }
+
+    public static inline function thenGetOrElse<T>(promise:Promise<Maybe<T>>, value:T):Promise<T> {
+        return promise.then(x -> x.getOrElse(value));
+    }
+
+    public static inline function thenOrElse<T>(promise:Promise<Maybe<T>>, value:Maybe<T>):Promise<Maybe<T>> {
+        return promise.then(x -> x.orElse(value));
+    }
+
+    public static inline function thenIsEmpty<T>(promise:Promise<Maybe<T>>):Promise<Bool> {
+        return promise.then(x -> x.isEmpty());
+    }
+
+    public static inline function thenNonEmpty<T>(promise:Promise<Maybe<T>>):Promise<Bool> {
+        return promise.then(x -> x.nonEmpty());
+    }
+
     public static inline function thenMap<T, U>(promise:Promise<Maybe<T>>, fn:PromiseHandler<T, U>):Promise<Maybe<U>> {
         return promise.then(x -> x.map(cast fn));
     }
@@ -31,6 +63,10 @@ class MaybePromiseTools {
 
     public static inline function thenFold<T, U>(promise:Promise<Maybe<T>>, ifEmpty:PromiseHandler0<T, U>, fn:PromiseHandler<T, U>):Promise<U> {
         return promise.then(x -> x.fold(ifEmpty, cast fn));
+    }
+
+    public static inline function thenIter<T>(promise:Promise<Maybe<T>>, fn:(value:T) -> Void):Promise<Void> {
+        return promise.then(x -> x.iter(fn));
     }
 
     public static inline function thenMatch<T>(promise:Promise<Maybe<T>>, fn:PromiseHandler<T, Void>, ifEmpty:PromiseHandler0<T, Void>):Promise<Void> {
--- test/expromise/MaybePromiseToolsSuite.hx
+++ test/expromise/MaybePromiseToolsSuite.hx
@@ -1,11 +1,152 @@
 package expromise;
 
 import extype.Maybe;
+import extype.NoDataException;
 
 using expromise.MaybePromiseTools;
 
 class MaybePromiseToolsSuite extends BuddySuite {
     public function new() {
+        describe("MaybePromiseTools.thenToOption()", {
+            it("should convert to Some(value)", done -> {
+                Promise.resolve(Maybe.of(100)).thenToOption().then(x -> switch (x) {
+                    case Some(v):
+                        v.should.be(100);
+                        done();
+                    case None:
+                        fail();
+                });
+            });
+
+            it("should convert to None", done -> {
+                Promise.resolve(Maybe.empty()).thenToOption().then(x -> switch (x) {
+                    case Some(v):
+                        fail();
+                    case None:
+                        done();
+                });
+            });
+        });
+
+        describe("MaybePromiseTools.thenGet()", {
+            it("should convert to value", done -> {
+                Promise.resolve(Maybe.of("hello")).thenGet().then(x -> {
+                    x.should.be("hello");
+                    done();
+                });
+            });
+
+            it("should convert to null", done -> {
+                Promise.resolve(Maybe.empty()).thenGet().then(x -> {
+                    (x:Null<Any>).should.be(null);
+                    done();
+                });
+            });
+        });
+
+        describe("MaybePromiseTools.thenGetUnsafe()", {
+            it("should convert to value", done -> {
+                Promise.resolve(Maybe.of(100)).thenGetUnsafe().then(x -> {
+                    x.should.be(100);
+                    done();
+                });
+            });
+        });
+
+        describe("MaybePromiseTools.thenGetOrThrow()", {
+            it("should convert to value", done -> {
+                Promise.resolve(Maybe.of(100)).thenGetOrThrow().then(x -> {
+                    x.should.be(100);
+                    done();
+                });
+            });
+
+            it("should be rejected", done -> {
+                Promise.resolve(Maybe.empty()).thenGetOrThrow().catchError(e -> {
+                    Std.isOfType(e, NoDataException).should.be(true);
+                    done();
+                });
+            });
+
+            it("should be rejected", done -> {
+                Promise.resolve(Maybe.empty()).thenGetOrThrow(() -> "error").catchError(e -> {
+                    (e : Exception).message.should.be("error");
+                    done();
+                });
+            });
+        });
+
+        describe("MaybePromiseTools.thenGetOrElse()", {
+            it("should return value", done -> {
+                Promise.resolve(Maybe.of(100)).thenGetOrElse(-1).then(x -> {
+                    x.should.be(100);
+                    done();
+                });
+            });
+
+            it("should return alt value", done -> {
+                Promise.resolve(Maybe.empty()).thenGetOrElse(-1).then(x -> {
+                    x.should.be(-1);
+                    done();
+                });
+            });
+        });
+
+        describe("MaybePromiseTools.thenOrElse()", {
+            it("should return value", done -> {
+                Promise.resolve(Maybe.of(100)).thenOrElse(Maybe.of(-1)).thenIter(x -> {
+                    x.should.be(100);
+                    done();
+                });
+            });
+
+            it("should return alt value", done -> {
+                Promise.resolve(Maybe.empty()).thenOrElse(Maybe.of(-1)).thenIter(x -> {
+                    x.should.be(-1);
+                    done();
+                });
+            });
+
+            it("should return empty", done -> {
+                Promise.resolve(Maybe.empty()).thenOrElse(Maybe.empty()).then(x -> {
+                    x.isEmpty().should.be(true);
+                    done();
+                });
+            });
+        });
+
+        describe("MaybePromiseTools.thenIsEmpty()", {
+            it("should return false", done -> {
+                Promise.resolve(Maybe.of(100)).thenIsEmpty().then(x -> {
+                    x.should.be(false);
+                    done();
+                });
+            });
+
+            it("should return true", done -> {
+                Promise.resolve(Maybe.empty()).thenIsEmpty().then(x -> {
+                    x.should.be(true);
+                    done();
+                });
+            });
+        });
+
+        describe("MaybePromiseTools.thenNonEmpty()", {
+            it("should return false", done -> {
+                Promise.resolve(Maybe.of(100)).thenNonEmpty().then(x -> {
+                    x.should.be(true);
+                    done();
+                });
+            });
+
+            it("should return true", done -> {
+                Promise.resolve(Maybe.empty()).thenNonEmpty().then(x -> {
+                    x.should.be(false);
+                    done();
+                });
+            });
+        });
+
         describe("MaybePromiseTools.thenMap()", {
             it("should map to U", done -> {
                 Promise.resolve(Maybe.of(100)).thenMap(x -> x * 2).then(x -> {
@@ -121,6 +262,20 @@ class MaybePromiseToolsSuite extends BuddySuite {
                     x.should.be(200);
                     done();
                 });
+            });
+        });
+
+        describe("MaybePromiseTools.thenIter()", {
+            it("should call", done -> {
+                Promise.resolve(Maybe.of(100)).thenIter(x -> {
+                    x.should.be(100);
+                    done();
+                });
+            });
+
+            it("should never call", done -> {
+                Promise.resolve(Maybe.empty()).thenIter(_ -> fail());
+                wait(5, done);
             });
         });
 
