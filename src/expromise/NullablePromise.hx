59668feab5cb5dc6325c2c94390fb36a89ce5483
s_tomikawa <s_tomikawa@denkiyagi.jp>
2020-10-08 14:05:28 +0900

    Fix static funcs to methods

--- src/expromise/NullablePromise.hx
+++ src/expromise/NullablePromise.hx
@@ -6,60 +6,64 @@ import extype.Maybe;
 import extype.Nullable;
 
 abstract NullablePromise<T>(Promise<Nullable<T>>) from Promise<Nullable<T>> to Promise<Nullable<T>> {
-    public static inline function thenToMaybe<T>(promise:NullablePromise<T>):Promise<Maybe<T>> {
-        return promise.then(x -> x.toMaybe());
+    public inline extern function new(executor:(Nullable<T>->Void)->(Dynamic->Void)->Void) {
+        this = new Promise(executor);
     }
 
-    public static inline function thenIsEmpty<T>(promise:NullablePromise<T>):Promise<Bool> {
-        return promise.then(x -> x.isEmpty());
+    public inline function thenToMaybe<T>():Promise<Maybe<T>> {
+        return this.then(x -> x.toMaybe());
     }
 
-    public static inline function thenNonEmpty<T>(promise:NullablePromise<T>):Promise<Bool> {
-        return promise.then(x -> x.nonEmpty());
+    public inline function thenIsEmpty<T>():Promise<Bool> {
+        return this.then(x -> x.isEmpty());
     }
 
-    public static inline function thenGet<T>(promise:NullablePromise<T>):Promise<Null<T>> {
-        return promise.then(x -> x.get());
+    public inline function thenNonEmpty<T>():Promise<Bool> {
+        return this.then(x -> x.nonEmpty());
     }
 
-    public static inline function thenGetUnsafe<T>(promise:NullablePromise<T>):Promise<T> {
-        return promise.then(x -> x.getUnsafe());
+    public inline function thenGet<T>():Promise<Null<T>> {
+        return this.then(x -> x.get());
     }
 
-    public static inline function thenGetOrThrow<T>(promise:NullablePromise<T>, ?errorFn:() -> Dynamic):Promise<T> {
-        return promise.then(x -> x.getOrThrow(errorFn));
+    public inline function thenGetUnsafe<T>():Promise<T> {
+        return this.then(x -> x.getUnsafe());
     }
 
-    public static inline function thenGetOrElse<T>(promise:NullablePromise<T>, value:T):Promise<T> {
-        return promise.then(x -> x.getOrElse(value));
+    public inline function thenGetOrThrow<T>(?errorFn:() -> Dynamic):Promise<T> {
+        return this.then(x -> x.getOrThrow(errorFn));
     }
 
-    public static inline function thenOrElse<T>(promise:NullablePromise<T>, value:Nullable<T>):NullablePromise<T> {
-        return promise.then(x -> x.orElse(value));
+    public inline function thenGetOrElse<T>(value:T):Promise<T> {
+        return this.then(x -> x.getOrElse(value));
     }
 
-    public static inline function thenMap<T, U>(promise:NullablePromise<T>, fn:PromiseHandler<T, U>):Promise<Nullable<U>> {
-        return promise.then(x -> x.map(cast fn));
+    public inline function thenOrElse<T>(value:Nullable<T>):NullablePromise<T> {
+        return this.then(x -> x.orElse(value));
     }
 
-    public static inline function thenFlatMap<T, U>(promise:NullablePromise<T>, fn:PromiseHandler<T, Nullable<U>>):Promise<Nullable<U>> {
-        return promise.then(x -> x.flatMap(cast fn));
+    public inline function thenMap<T, U>(fn:PromiseHandler<T, U>):NullablePromise<U> {
+        return this.then(x -> x.map(cast fn));
     }
 
-    public static inline function thenHas<T>(promise:NullablePromise<T>, value:T):Promise<Bool> {
-        return promise.then(x -> x.has(value));
+    public inline function thenFlatMap<T, U>(fn:PromiseHandler<T, Nullable<U>>):NullablePromise<U> {
+        return this.then(x -> x.flatMap(cast fn));
     }
 
-    public static inline function thenExists<T>(promise:NullablePromise<T>, fn:T->Bool):Promise<Bool> {
-        return promise.then(x -> x.exists(fn));
+    public inline function thenHas<T>(value:T):Promise<Bool> {
+        return this.then(x -> x.has(value));
     }
 
-    public static inline function thenFind<T>(promise:NullablePromise<T>, fn:T->Bool):Promise<Null<T>> {
-        return promise.then(x -> x.find(fn));
+    public inline function thenExists<T>(fn:T->Bool):Promise<Bool> {
+        return this.then(x -> x.exists(fn));
     }
 
-    public static inline function thenFilter<T>(promise:NullablePromise<T>, fn:PromiseHandler<T, Bool>):NullablePromise<T> {
-        return promise.then((value -> {
+    public inline function thenFind<T>(fn:T->Bool):Promise<Null<T>> {
+        return this.then(x -> x.find(fn));
+    }
+
+    public inline function thenFilter<T>(fn:PromiseHandler<T, Bool>):NullablePromise<T> {
+        return this.then((value -> {
             value.fold(() -> Promise.resolve(Nullable.empty()), x -> {
                 final ret = fn.call(x);
                 if (EqualsTools.strictEqual(ret, true)) {
@@ -74,16 +78,16 @@ abstract NullablePromise<T>(Promise<Nullable<T>>) from Promise<Nullable<T>> to P
         } : PromiseHandler<Nullable<T>, Nullable<T>>));
     }
 
-    public static inline function thenFold<T, U>(promise:NullablePromise<T>, ifEmpty:PromiseHandler0<T, U>, fn:PromiseHandler<T, U>):Promise<U> {
-        return promise.then(x -> x.fold(ifEmpty, cast fn));
+    public inline function thenFold<T, U>(ifEmpty:PromiseHandler0<T, U>, fn:PromiseHandler<T, U>):Promise<U> {
+        return this.then(x -> x.fold(ifEmpty, cast fn));
     }
 
-    public static inline function thenIter<T>(promise:NullablePromise<T>, fn:(value:T) -> Void):Promise<Void> {
-        return promise.then(x -> x.iter(fn));
+    public inline function thenIter<T>(fn:(value:T) -> Void):Promise<Void> {
+        return this.then(x -> x.iter(fn));
     }
 
-    public static inline function thenMatch<T>(promise:NullablePromise<T>, fn:PromiseHandler<T, Void>, ifEmpty:PromiseHandler0<T, Void>):Promise<Void> {
-        return promise.then(x -> x.match(cast fn, ifEmpty));
+    public inline function thenMatch<T>(fn:PromiseHandler<T, Void>, ifEmpty:PromiseHandler0<T, Void>):Promise<Void> {
+        return this.then(x -> x.match(cast fn, ifEmpty));
     }
 
     public static function resolveOf<T>(x:T):NullablePromise<T> {
